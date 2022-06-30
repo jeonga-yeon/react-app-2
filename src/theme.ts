b6335a6484cfb2dbf2b32d5b3482d41ac4ee15b0
unknown <wings2132@naver.com>
2022-06-30 16:17:33 +0900

    commit

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  textColor: "white",
-  bgColor: "#2f3640",
-  accentColor: "#9c88ff",
-  cardBgColor: "transparent",
+  bgColor: "#3F8CF2",
+  boardColor: "#DADFE9",
+  cardColor: "white",
 };
